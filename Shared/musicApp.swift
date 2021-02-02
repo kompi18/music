f36846370d293a35661bd8ef951f23b8e277b27f
p.m.student <mc.spade77311@gmail.com>
2021-02-02 12:33:38 -0600

    Initial Commit

---
+++ Shared/musicApp.swift
@@ -0,0 +1,17 @@
+//
+//  musicApp.swift
+//  Shared
+//
+//  Created by P.M. Student on 2/2/21.
+//
+
+import SwiftUI
+
+@main
+struct musicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
